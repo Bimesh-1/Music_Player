1dc293832f8281163abb5fa13f281c19f446c7ed
Upreti Bimesh <s2400076@BC-C02GR0C3Q6LRs-MacBook-Air.local>
2024-09-24 10:31:52 +0300

    uploading music player

---
+++ src/components/PlayerControls.jsx
@@ -0,0 +1,30 @@
+import { useMusicPlayer } from "../hooks/useMusicPlayer";
+import PlayArrow from "@mui/icons-material/PlayArrow";
+
+
+
+const PlayerControls = () => {
+    const music = useMusicPlayer();
+
+    return (
+
+        <>
+            <div>
+                <p>{music.currentTrackName}</p>
+            </div>
+            <button onClick={music.playPreviousTrack}>Previous</button>
+            <button onClick={music.togglePlay}>
+                {music.isPlaying ? "Pause" : <PlayArrow />}
+            </button>
+            <button onClick={music.playNextTrack}>Next</button>
+            <button onClick={music.playNextTrack}>Next</button>
+
+
+        </>
+
+
+    )
+
+};
+
+export { PlayerControls };
